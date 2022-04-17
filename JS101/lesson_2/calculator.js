d0819e7995b795fd46aa14d3fed910b7aa8419bd
Fred Coble <coblefred@gmail.com>
2022-04-17 18:31:32 -0400

    Three Calculator Bonus Exercises in Lesson 2

--- JS101/lesson_2/calculator.js
+++ JS101/lesson_2/calculator.js
@@ -4,32 +4,71 @@
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
 const readline = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json')
 
-console.log('Welcome to the Calculator!');
-
-console.log("What's the first number?");
-let number1 = readline.question();
-
-console.log("What's the second number?");
-let number2 = readline.question();
-
-console.log(`${number1} ${number2}`);
-
-console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide ?');
-let operation = readline.question();
-
-let output;
-if (operation === '1') { // '1' represents addition
-  output = Number(number1) + Number(number2);
-} else if (operation === '2') { // '2' represents subtraction
-  output = Number(number1) - Number(number2);
-} else if (operation === '3') { // 3 represents multiplication
-  output = Number(number1) * Number(number2);
-} else if (operation === '4') { // 4 represents division
-  output = Number(number1) / Number(number2);
+function messages(message, lang='en') {
+  return MESSAGES[lang][message];
 }
 
-console.log(`The result is: ${output}`);
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
+function invalidNumber(number) {
+  /* use trimStart to trim the leading whitespace, if nothing is left,
+   * we ensure that we don't return a 0 from the Number() check on an empty
+   * string */
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
 
+while(true) {
+  prompt(messages('welcome','es'));
 
+  prompt(messages('firstNumber','es'));
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    prompt(messages('invalidNumber','es'));
+    number1 = readline.question();
+  }
+
+  prompt(messages('secondNumber','es'));
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    prompt(messages('invalidNumber','es'));
+    number2 = readline.question();
+  }
+
+  prompt(messages('operation','es'));
+  let operation = readline.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(messages('errorOperation','es'));
+    operation = readline.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`${messages('result','es')} ${output}`);
+
+  prompt(messages('again','es'));
+  let continueCalculating = readline.question();
+
+  if (continueCalculating[0].toLowerCase() !== 'y') break;
+
+} 
